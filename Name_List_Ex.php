e8733817041d480c8780ba8cfc9026b9a807c1b5
Jason <walzak@gmail.com>
2017-01-17 19:41:00 -0500

    Initial Commit
    The start of a group project.

---
+++ Name_List_Ex.php
@@ -0,0 +1,17 @@
+<?php
+
+    class name_list extends input_data {
+    //Four arrays of first names, and last names for two genders.
+        
+    //http://fantasywritersresource.blogspot.ca/2012/09/beautifiul-rare-and-unique-female-names.html
+
+    //http://medieval.stormthecastle.com/medieval-names.htm
+
+    $first_name_f = arr("Leanne", "Sonya", "Heather", "Glenda", "Abella", "Aililve", "Alura", "Calista", "Carita", "Cein" "Chrissanth", "Cleantha", "Delica", "Deva", "Elidie", "Evvie", "Eliska", "Faye", "Faylinn", "Gaea");
+    $first_name_m = arr("Isaiah", "Jason", "Matt", "Qole", "Merek", "Carac", "Ulric", "Rowan", "Althalos", "Brom", "Grom", "Jarin", "Joturn", "Asher", "Frodo" );
+    $last_name_f = arr();
+    $last_name_m = arr();
+}
+
+
+?>
---
+++ input_data.php
@@ -0,0 +1,22 @@
+<?php
+	class Input_Data{
+
+        //Initial property declaration
+        public $char_f_name;
+        public $char_l_name;
+        public $char_class;
+        public $char_race;
+        public $char_history;
+        public $strength;
+        public $dexterity;
+        public $constitution;
+        public $wisdom;
+        public $intelligence;
+        public $charisma;
+
+        
+
+	}
+
+
+?>
