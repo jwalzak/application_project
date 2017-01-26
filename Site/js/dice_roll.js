39e0bc4fed44f746cfcba811a8d373cc23101f6b
Jason <walzak@gmail.com>
2017-01-25 20:03:32 -0500

    Dice rolling functions
    Functions for rolling four six sided dice.
    Function for rolling any die of any size.

--- Site/js/dice_roll.js
+++ Site/js/dice_roll.js
@@ -1,14 +1,31 @@
 let die1, die2, die3, die4;
 
-
+//Generates a random number for what ever number of sided dies we want 
+//This will keep it expandable if we want to open it up to other dice games
 function dice(num) {
    let roll = Math.floor((Math.random() * num) + 1);
    return roll;
 }
 
 function diceRoll(){
-    die1 = diceRoll(6);
-    die2 = diceRoll(6);
-    die3 = diceRoll(6);
-    die4 = diceRoll(6);
+    //Roll each die individually, should probably be an array
+    die1 = dice(6);
+    die2 = dice(6);
+    die3 = dice(6);
+    die4 = dice(6);
+    //Put the dice in an array
+    let rolledDice = [die1, die2, die3, die4];
+    //Sort from largest to smallest
+    rolledDice.sort(function(a,b){return b-a});
+    //Remove the smallest rolled die
+    rolledDice.splice(-1, 1);
+    console.log("Four rolls");
+    console.log(die1);
+    console.log(die2);
+    console.log(die3);
+    console.log(die4);
+    console.log("For loop");
+    for(let i = 0; i<rolledDice.length; i++){
+        console.log(rolledDice[i]);
+    }
 }
