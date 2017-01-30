6eda9dc32c2a800f0eb4cc71c04c8e4e1aa993d4
IsaiahBekkers <IsaiahBekkers@isaiahs-mbp.fc.ca>
2017-01-30 14:19:24 -0500

    Added navbar.php
    Created navbar.php as a separate file under “Site”

---
+++ Site/navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Hero Burger &#x2605; Made to Order!</title>
+
+	<link rel="stylesheet" href="css/subcontent.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed|Permanent+Marker|Crete+Round|Droid+Serif|Roboto" rel="stylesheet">
+	<script src="https://use.fontawesome.com/97f2d469d8.js"></script>
+</head>
+
+<!-- navigation  -->
+<nav>
+	<ul>
+		<li><a href="account.php">Create an Account</a></li>
+		<li><a href="charvault.php">Character Vault</a></li>
+		<li><a href="creation.php">Hero Creation</a></li>
+		<li><a href="summary.php">Hero Summary</a></li>
+		<li><a href="about.php">About</a></li>
+	</ul>
+</nav>
