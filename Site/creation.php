e3f972af81432e101647bf087fc7d48a467f4203
maxcassettes <mattcossette@gmail.com>
2017-01-31 13:33:16 -0500

    METHOD!!!!

--- Site/creation.php
+++ Site/creation.php
@@ -4,7 +4,7 @@ require("navbar.php");
 require_once('input_data.php');
 require_once('character.php');
 
-	if($_SERVER['REQUEST_MEHTOD'] == 'POST' && !empty($_POST['f_name']) && !empty($_POST['l_name']) && !empty($_POST['backstory'])) {
+	if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['f_name']) && !empty($_POST['l_name']) && !empty($_POST['backstory'])) {
 		$SESSION['f_name'] = $_POST['f_name'];
 		$SESSION['l_name'] = $_POST['l_name'];
 		$SESSION['backstory'] = $POST['backstory'];
