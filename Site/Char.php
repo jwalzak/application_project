080afcee83eaf718f70b74d33d19d0c2d52fd6ff
maxcassettes <mattcossette@gmail.com>
2017-01-31 13:20:11 -0500

    New Char.php class file
    Using public properties instead of private and getters/setters for now.

---
+++ Site/Char.php
@@ -0,0 +1,23 @@
+<?php 
+
+require("connect.php"); 
+$conn->select_db("heroschema");
+    
+        class Character {
+
+        public $char_f_name;
+        public  $char_l_name;
+        public  $char_class;
+        public  $char_race;
+        public $char_gender; 
+        public $char_history;
+        public  $strength;
+        public  $dexterity;
+        public  $constitution;
+        public  $wisdom;
+        public  $intelligence;
+        public  $charisma;
+
+        }
+
+        ?>
