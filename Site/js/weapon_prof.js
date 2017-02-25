3348bd98d50477487d4451b78cfebcdb61a1de45
IsaiahBekkers <IsaiahBekkers@Isaiahs-Mac.local>
2017-02-25 16:32:28 -0500

    Weapon Proficiencies - Text
    Simple text explaining current weapon proficiencies at level.
    
    - until we decide whether we want level editing or independent custom
    stats

--- Site/js/weapon_prof.js
+++ Site/js/weapon_prof.js
@@ -3,3 +3,16 @@
  *  Bonuses 14, 173-174
  *  
  */
+
+//WEAPON PROFICIENCEIES 
+//In D&D5e Weapon Proficiencies (Bonus Attack stat earned if a character is considered "proficient" with their type of weapon [simple or martial]) are based on level. Any new character (0 exp) is considered level 1. Proficiencies are listed as follows...
+
+/**
+ * WEAPON PROFICIENCIES
+ * Levels 1 - 4 (inclusive):   +2 Proficiency Bonus
+ * Levels 5 - 8 (inclusive):   +3 Proficiency Bonus
+ * Levels 9 - 12 (inclusive):  +4 Proficiency Bonus
+ * Levels 13 - 16 (inclusive): +5 Proficiency Bonus
+ * Levels 17 - 20 (inclusive): +6 Proficiency Bonus
+ */
+
